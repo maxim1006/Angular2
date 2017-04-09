4493bdc54dd87f45ed9dcefecc844709c3aaa764
Max <maxim1006@list.ru>
2017-04-09 22:19:15 +0300

    updated examples

--- test-proper-project/src/scripts/modules/m-framework/components/parent-component/parentComponent.ts
+++ test-proper-project/src/scripts/modules/m-framework/components/parent-component/parentComponent.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
+import {ChildComponent} from "./child-component/childComponent";
 
 @Component({
     selector: "parent-component",
@@ -6,5 +7,17 @@ import { Component } from "@angular/core";
 })
 
 export class ParentComponent {
+
+    //так могу находить любой компонент в темплейте и дергать его апи, тоже самое, что и с #child, только с локальной переменной я могу это сделать только в шаблоне, а так могу и в контроллере
+    @ViewChild(ChildComponent)
+    private childComponent: ChildComponent;
+
+    public childStart() {
+        this.childComponent.start();
+    }
+
+    public childStop() {
+        this.childComponent.stop();
+    }
 
 }
