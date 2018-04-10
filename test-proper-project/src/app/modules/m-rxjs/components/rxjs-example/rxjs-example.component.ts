26da1d2b8687ab1c57941cf85fe3ab26ea3cea94
Max <maxim1006@list.ru>
2018-04-10 13:03:48 +0300

    added rxjs examples

--- test-proper-project/src/app/modules/m-rxjs/components/rxjs-example/rxjs-example.component.ts
+++ test-proper-project/src/app/modules/m-rxjs/components/rxjs-example/rxjs-example.component.ts
@@ -4,7 +4,9 @@ import {Subscriber} from "rxjs/Subscriber";
 import {Subscription} from "rxjs/Subscription";
 import {domenToken} from "../../../shared/tokens/tokens";
 import {HttpClient} from "@angular/common/http";
-import {concatAll, concatMap, map} from "rxjs/operators";
+import {concatMap, map, mergeAll, switchAll, concatAll, combineAll, combineLatest} from "rxjs/operators";
+import {of} from "rxjs/observable/of";
+import {forkJoin} from "rxjs/observable/forkJoin";
 
 @Component({
     selector: 'rxjs-example',
@@ -40,7 +42,7 @@ export class RxjsExampleComponent implements OnInit, OnDestroy {
         // });
 
         //Запросы по нескольким id в порядке очереди приходят, сабскрйбится к каждому обзерваблу в отдельности, дождется выполнения первого и лишь затем второго, сработает когда все придут
-        let idsObservable$ = Observable.from([0,1,2]);
+        //let idsObservable$ = Observable.from([0,1,2]);
 
         // let idsObservableInOrder$ = idsObservable$.pipe(
         //     map((id) => {
@@ -61,18 +63,91 @@ export class RxjsExampleComponent implements OnInit, OnDestroy {
 
 
         //если нужно дождаться 1го и потом 2ой делай так
-        let queueObservable$ = this._http.get(`${domenToken}mocks.json`).pipe(
-            concatMap((urls: any) => {
-                return self._http.get(`${domenToken}${urls.familyUrl}`)
-            },
-(first, second) => {return {first, second}})
-        ).subscribe(({first, second}) => {
-            console.log(first);
-            console.log(second);
-        });
+//         let queueObservable$ = this._http.get(`${domenToken}mocks.json`).pipe(
+//             concatMap((urls: any) => {
+//                 return self._http.get(`${domenToken}${urls.familyUrl}`)
+//             },
+// (first, second) => {return {first, second}})
+//         ).subscribe(({first, second}) => {
+//             console.log(first);
+//             console.log(second);
+//         });
 
 
-        //switchMap - убивает предыдущий обзервбл и создает новый (как пример с автокомплитом)
+
+        const source = of(
+            this._http.get(`${domenToken}family0.json`),
+            this._http.get(`${domenToken}family1.json`),
+            this._http.get(`${domenToken}family2.json`));
+
+        // const concatedAll$ = source.pipe(concatAll());
+        //
+        // concatedAll$.subscribe((data) => {
+        //    console.log(data);
+        //    //(3) [{…}, {…}, {…}]
+        //    //(3) [{…}, {…}, {…}]
+        //    //(3) [{…}, {…}, {…}]
+              //подписываемся к каждому обзервблу, когда заканчивается предыдущий и получаем для каждого console.log
+        // })
+
+
+
+        // const switchAll$ = source.pipe(switchAll());
+        //
+        // switchAll$.subscribe((data) => {
+        //    console.log(data);
+        //    //(3) [{…}, {…}, {…}] - только результат последнего запроса family2.json
+        // })
+
+
+
+        // const mergeAll$ = source.pipe(mergeAll());
+        //
+        // mergeAll$.subscribe((data) => {
+        //     console.log(data);
+        //     //(3) [{…}, {…}, {…}]
+        //     //    //(3) [{…}, {…}, {…}]
+        //     //    //(3) [{…}, {…}, {…}] - подписка сразу ко всем обзервблам
+        // })
+
+
+
+        // const combineAll$ = source.pipe(combineAll());
+        //
+        // combineAll$.subscribe((data) => {
+        //     console.log(data);
+        //     //(3) [Array(3), Array(3), Array(3)] - результат сразу 3 запроса в 1 ом
+        // })
+
+
+        
+        // const intervalOne$ = Observable.interval(1000);
+        // const intervalTwo$ = Observable.interval(2000);
+        //
+        // Observable.combineLatest(
+        //     intervalTwo$,
+        //     intervalTwo$
+        // ).subscribe(all => console.log(all));
+        //
+        // //либо с запросами
+        // Observable.combineLatest(
+        //     this._http.get(`${domenToken}family0.json`),
+        //     this._http.get(`${domenToken}family1.json`),
+        //     this._http.get(`${domenToken}family2.json`)
+        // ).subscribe(all => console.log(all));
+
+        // this._http.get(`${domenToken}family0.json`).pipe(
+        //     combineLatest(this._http.get(`${domenToken}family1.json`), this._http.get(`${domenToken}family2.json`))
+        // ).subscribe(all => console.log(all));
+
+
+
+        forkJoin([
+            this._http.get(`${domenToken}family0.json`),
+            this._http.get(`${domenToken}family1.json`),
+            this._http.get(`${domenToken}family2.json`)
+        ]).subscribe(all => console.log(all));
+
 
         // https://www.youtube.com/watch?v=QfvwQEJVOig&feature=youtu.be&t=1h12s
     }
