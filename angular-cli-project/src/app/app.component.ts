e496dce516df71bdd33a149c78881ef62dd8ea01
Max <maxim1006@list.ru>
2018-05-16 15:06:50 +0300

    added angular cli

--- angular-cli-project/src/app/app.component.ts
+++ angular-cli-project/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-component',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+    @HostBinding("class")
+    private appComponentClass = "app-component";
+
+    constructor() {
+    }
 }
