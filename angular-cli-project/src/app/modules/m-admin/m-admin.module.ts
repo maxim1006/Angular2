bba5d9b3d3fe469c5e9fe067215f5f9293c7d09f
mama1213 <maxim1006@list.ru>
2018-07-10 12:01:23 +0300

    update

--- angular-cli-project/src/app/modules/m-admin/m-admin.module.ts
+++ angular-cli-project/src/app/modules/m-admin/m-admin.module.ts
@@ -2,21 +2,50 @@ import {NgModule} from "@angular/core";
 import {SharedModule} from "../shared/shared.module";
 import {MAdminComponent} from "./m-admin.component";
 import {MAdminGuardService} from "./m-admin-guard.service";
-import {AdminRoutingModule} from "./admin-routing.module";
 import {AdminIdComponent} from "./components/admin-id.component";
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MAdminPopupComponent} from "./components/admin-popup.component";
+import {RouterModule, Routes} from "@angular/router";
+import {MRxjsComponent} from "../m-rxjs/m-rxjs.component";
+
+
+const routes: Routes = [
+    {
+        path: "admin",
+        component: MAdminComponent,
+        children: [
+            {
+                path: "",
+                pathMatch: "full",
+                redirectTo: "default"
+            },
+            {
+                path: 'default',
+                component: AdminIdComponent,
+                data: {
+                    title: "Admin"
+                },
+            },
+            {path: ':id', component: AdminIdComponent},
+            {path: 'adminPopup', component: MAdminPopupComponent, outlet: "adminPopup"
+            }
+        ]
+    }
+];
+
 
 @NgModule({
     imports: [
         SharedModule,
-        AdminRoutingModule,
         FormsModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        RouterModule.forChild(routes),
     ],
     declarations: [
         MAdminComponent,
-        AdminIdComponent
+        AdminIdComponent,
+        MAdminPopupComponent
     ],
     providers: [MAdminGuardService]
 })
