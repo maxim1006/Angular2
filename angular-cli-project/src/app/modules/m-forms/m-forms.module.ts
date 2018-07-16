12f4ac88e71c2ee62008f5bd3ccbfb9b09662750
mama1213 <maxim1006@list.ru>
2018-07-16 11:13:04 +0300

    update

--- angular-cli-project/src/app/modules/m-forms/m-forms.module.ts
+++ angular-cli-project/src/app/modules/m-forms/m-forms.module.ts
@@ -1,10 +1,9 @@
-import {NgModule} from "@angular/core";
-import {HttpModule} from "@angular/http";
-import {CommonModule} from "@angular/common";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {TField} from "./components/t-field/t-field.component";
-import {MFormComponent} from "./m-form.component";
-import {SharedModule} from "../shared/shared.module";
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TField} from './components/t-field/t-field.component';
+import {MFormComponent} from './m-form.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     imports: [SharedModule, HttpModule, FormsModule, ReactiveFormsModule],
