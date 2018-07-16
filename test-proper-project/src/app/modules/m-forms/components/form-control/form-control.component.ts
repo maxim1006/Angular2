aa82ef466b9796a134f311da65facc095e02898d
mama1213 <maxim1006@list.ru>
2018-07-16 10:32:29 +0300

    update

---
+++ test-proper-project/src/app/modules/m-forms/components/form-control/form-control.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'm-form-control',
+    templateUrl: ``
+})
+
+export class MFormControlComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
--- test-proper-project/src/app/modules/m-forms/m-forms.module.ts
+++ test-proper-project/src/app/modules/m-forms/m-forms.module.ts
@@ -1,16 +1,25 @@
-import {NgModule} from "@angular/core";
+import {NgModule, forwardRef} from "@angular/core";
 import {HttpModule} from "@angular/http";
 import {CommonModule} from "@angular/common";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
 import {TField} from "./components/t-field/t-field.component";
 import {MFormComponent} from "./m-form.component";
 import {SharedModule} from "../shared/shared.module";
+import {MFormControlComponent} from "./components/form-control/form-control.component";
+
+//ControlValueAccessor - его надо реализовать, чтобы получить кастомный формКонтрол
 
 @NgModule({
     imports: [SharedModule, HttpModule, FormsModule, ReactiveFormsModule],
-    declarations: [MFormComponent, TField],
+    declarations: [MFormComponent, TField, MFormControlComponent],
     exports: [MFormComponent],
     providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            // useExisting: MFormControlComponent,
+            useExisting: forwardRef(() => MFormControlComponent), //- делаю так если задаю класс после вызова, чтобы избежать ошибки компиляции
+            multi: true
+        }
     ]
 })
 export class MFormsModule {
