a16c3ae924f2e9282a7592f1a9b836672c31c661
mama1213 <maxim1006@list.ru>
2018-07-16 11:16:41 +0300

    update

--- angular-cli-project/src/app/modules/m-forms/m-forms.module.ts
+++ angular-cli-project/src/app/modules/m-forms/m-forms.module.ts
@@ -1,16 +1,22 @@
-import {NgModule} from '@angular/core';
-import {HttpModule} from '@angular/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TField} from './components/t-field/t-field.component';
-import {MFormComponent} from './m-form.component';
-import {SharedModule} from '../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TField } from './components/t-field/t-field.component';
+import { MFormComponent } from './m-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     imports: [SharedModule, HttpModule, FormsModule, ReactiveFormsModule],
     declarations: [MFormComponent, TField],
     exports: [MFormComponent],
     providers: [
-    ]
+        {
+            provide: NG_VALUE_ACCESSOR,
+            // useExisting: MFormControlComponent,
+            useExisting: forwardRef(() => MFormControlComponent), //- делаю так если задаю класс после вызова, чтобы избежать ошибки компиляции
+            multi: true
+        }
+    ]	     ]
 })
 export class MFormsModule {
 }
